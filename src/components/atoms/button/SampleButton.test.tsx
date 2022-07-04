d2478857e1e9ef3b5c5857770b9ddf9ecc187826
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-07-04 12:44:43 +0900

    テストケース追加

---
+++ src/components/atoms/button/SampleButton.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { composeStories } from "@storybook/testing-react";
+import * as stories from "./SampleButton.stories";
+
+/**
+ * StorybookのStoryをインポートした後、
+ * composeStoriesを利用してテストで利用できる
+ * コンポーネントへ変換する。
+ */
+
+const { Default } = composeStories(stories);
+
+/**
+ * テストケース
+ */
+test("render button with default args", () => {
+  render(<Default>Button</Default>);
+  const buttonElement = screen.getByText(/Button/i);
+  expect(buttonElement).not.toBeNull();
+});
